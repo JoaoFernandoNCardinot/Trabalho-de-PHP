0259ce41bbf1362aa38532dcfc21633f640eb55f
João Fernando <jfnetto06@gmail.com>
2017-10-13 12:28:59 -0300

    show

--- home.php
+++ home.php
@@ -24,13 +24,33 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Home</title>
+		<title>Home|SINEP</title>
+		<meta charset="utf-8"/>
 		<link rel="stylesheet" type="text/css" href="./css/home.css"/>
+		<script src="./js/jquery.min.js"></script>
+		<script>
+			$(function(){
+
+				$(".cabecalho").append($("<div />").addClass("menu").append($("<ul />").append($("<li />").attr("id","amigos").text("MOSTRAR AMIGOS")).append($("<li />").append($("<form/>").attr({"method":"POST" , "action": "#"}).append($("<input/>").attr({"type": "submit" , "class":"bmenu" , "value": "SAIR"}))))).slideUp());
+
+				$(".cabecalho").hover(function(){
+					$("#img").attr("src","./img/menuw.png");
+				})
+				$(".cabecalho").mouseleave(function(){
+					$("#img").attr("src", "./img/menub.png");
+					$(".menu").slideUp("fast");
+				})
+
+				$(".cabecalho #img").click(function(){
+					$(".menu").slideToggle("fast");
+				});
+			});
+		</script>
 		<link href="https://fonts.googleapis.com/css?family=Oxygen" rel="stylesheet">
 		<link href="https://fonts.googleapis.com/css?family=Quicksand|Rubik|Varela+Round" rel="stylesheet">
 		<style>
 			body{
-				background: url(<?php echo $user['fundo']; ?>) no-repeat fixed;
+				background: url(<?php echo $user['fundo'];?>) fixed;
 				background-size: 100%;
 				margin: 0 auto;
 				text-align: center;
@@ -46,7 +66,10 @@
 	<body>
 		<div class="cabecalho"/>
 			<h1 class="logo">SINEP</h1>
-			<h1 class="infologo">PERFIL DO USUÁRIO</h1>
+			<div class="infologo">
+				<h1>PERFIL DO USUÁRIO</h1>
+				<img id="img" class="menua" src="./img/menub.png" alt="menu"/>
+			</div>
 		</div>
 		<div class="perfil">			
 			<img class="imgPer" src="<?php echo $user['perfil'];?>"/>
